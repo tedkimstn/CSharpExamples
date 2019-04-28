cfdc9426dbd8200e63fed66085544bc694cf8563
Ted Kim <tedkimflo@gmail.com>
2019-04-28 00:19:33 -0700

    c

---
+++ FileIO1OProj/FileIO1OProg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleUI
+{
+
+    //=========
+    class Program
+    {
+
+        //----------
+        static void Main(string[] args)
+        {
+            string filePath = "./tc_fileio.txt";
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            lines.Add("Sue,Storm,www.stormy.com");
+
+            File.WriteAllLines(filePath, lines);
+
+            Console.ReadLine();
+        }
+    }
+}
