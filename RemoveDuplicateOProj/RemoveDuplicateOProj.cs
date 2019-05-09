85b940a72c4f8d8b71951649f59632472b4bc86d
Ted Kim <tedkimflo@gmail.com>
2019-05-08 21:44:12 -0700

    c

---
+++ RemoveDuplicateOProj/RemoveDuplicateOProj.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    const int _max = 1000000;
+    static void Main()
+    {
+        var s1 = Stopwatch.StartNew();
+        for (int i = 0; i < _max; i++)
+        {
+            string value = RemoveDuplicateChars("datagridviewtips");
+            if (value == null)
+            {
+                return;
+            }
+        }
+        s1.Stop();
+        var s2 = Stopwatch.StartNew();
+        for (int i = 0; i < _max; i++)
+        {
+            string value = RemoveDuplicateCharsFast("datagridviewtips");
+            if (value == null)
+            {
+                return;
+            }
+        }
+        s2.Stop();
+        Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 1000000) /
+            _max).ToString("0.00 ns"));
+        Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) /
+            _max).ToString("0.00 ns"));
+        Console.Read();
+    }
+
+    static string RemoveDuplicateChars(string key)
+    {
+        // See comments in first example.
+        string result = "";
+
+        foreach (char value in key)
+        {
+            if (result.IndexOf(value) == -1)
+            {
+                result += value;
+            }
+        }
+        return result;
+    }
+
+    static string RemoveDuplicateCharsFast(string key)
+    {
+        // Remove duplicate chars using char arrays.
+        int keyLength = key.Length;
+
+        // Store encountered letters in this array.
+        char[] table = new char[keyLength];
+        int tableLength = 0;
+
+        // Store the result in this array.
+        char[] result = new char[keyLength];
+        int resultLength = 0;
+
+        foreach (char value in key)
+        {
+            // Scan the table to see if the letter is in it.
+            bool exists = false;
+            for (int i = 0; i < tableLength; i++)
+            {
+                if (value == table[i])
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            // If the letter is new, add to the table and the result.
+            if (!exists)
+            {
+                table[tableLength] = value;
+                tableLength++;
+
+                result[resultLength] = value;
+                resultLength++;
+            }
+        }
+        // Return the string at this range.
+        return new string(result, 0, resultLength);
+    }
+}
---
+++ ReverseStringOProj/ReverseStringOProg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    public static string ReverseString(string s)
+    {
+        char[] array = s.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+    }
+
+    public static string ReverseStringDirect(string s)
+    {
+        char[] array = new char[s.Length];
+        int forward = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            array[forward++] = s[i];
+        }
+        return new string(array);
+    }
+
+    static void Main()
+    {
+        int sum = 0;
+        const int _max = 10000000;
+        Console.WriteLine(ReverseString("test"));
+        Console.WriteLine(ReverseStringDirect("test"));
+
+        // Version 1: reverse with ToCharArray.
+        var s1 = Stopwatch.StartNew();
+        for (int i = 0; i < _max; i++)
+        {
+            sum += ReverseString("programmingisfun").Length;
+        }
+        s1.Stop();
+
+        // Version 2: reverse with iteration.
+        var s2 = Stopwatch.StartNew();
+        for (int i = 0; i < _max; i++)
+        {
+            sum += ReverseStringDirect("programmingisfun").Length;
+        }
+        s2.Stop();
+
+        Console.WriteLine(s1.Elapsed.TotalMilliseconds);
+        Console.WriteLine(s2.Elapsed.TotalMilliseconds);
+    }
+}
